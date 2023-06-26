404fb5dbfe3fd2ae68bb8457c56eb8088691bdb5
Zeynep Alperen <123326315+zeyzeppp@users.noreply.github.com>
2023-06-26 15:34:12 +0300

    Add files via upload

---
+++ cember_client.py
@@ -0,0 +1,33 @@
+#!/usr/bşn/env python3
+# -*- coding: UTF-8 -*-
+
+import rospy
+from geometry_msgs import Twist
+from ros_python_uygulamalar.srv import Yaricap
+
+
+
+def cemberClient():
+
+    rospy.wait_for_service("yaricap_servis")
+
+    try:
+
+        r = float(input("yaricapi giriniz"))
+        cember_servis = rospy.ServiceProxy("yaricap_servis", Yaricap)
+        cevap = cember_servis(r)
+
+        return cevap.yaricap
+    
+
+    except rospy.ServiceException:
+
+        rospy.loginfo("Servis Hatasi !")
+
+
+
+cemberClient()
+
+
+    
+    
---
+++ cember_server.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import rospy
+from geometry_msgs.msg import Twist
+from ros_python_uygulamalar.srv import Yaricap
+
+
+def yaricapHareket(istek):
+
+    hiz = Twist()
+
+    lineer_hiz = 0.5
+    hiz.linear.x = lineer_hiz
+
+    r = istek.yaricap
+
+    # w(acial hiz) = v(hiz) / r(yaricap)
+
+
+    hiz.linear.z = (hiz.linear.x) / r
+
+
+    while(not rospy.is_shutdown()):
+
+        pub.publish(hiz)
+
+
+def cemberServer():
+
+    global pub
+    
+    rospy.init_node("cember_server_node")
+    rospy.Service("yaricap_servis", Yaricap, yaricapHareket)
+
+    pub = rospy.Publisher("cmd_vel", Twist, queue_size = 10)
+
+    rospy.spin()
+
+
+
+if __name__ == "__main__":
+
+    cemberServer()
